a53a1b767bf3555239bb77692b8718840fcd1244
Brian <varcyonsariougames@gmail.com>
2022-12-26 13:13:59 -0600

    Figured out Text Change on Tag

---
+++ Source/Code_UnMade/UnmadeAbilitySystemGlobals.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemGlobals.h"
+#include "UnmadeAbilitySystemGlobals.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CODE_UNMADE_API UUnmadeAbilitySystemGlobals : public UAbilitySystemGlobals
+{
+	GENERATED_BODY()
+	
+};
